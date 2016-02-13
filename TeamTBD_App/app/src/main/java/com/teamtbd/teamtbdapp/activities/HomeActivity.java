2817af105a78edfcba4d6b9e1e8286863b112316
ModoNoob <ModoNoob@hotmail.fr>
2016-02-13 05:37:19 -0500

    Ajout de Connections

--- TeamTBD_App/app/src/main/java/com/teamtbd/teamtbdapp/activities/HomeActivity.java
+++ TeamTBD_App/app/src/main/java/com/teamtbd/teamtbdapp/activities/HomeActivity.java
@@ -1,75 +1,40 @@
 package com.teamtbd.teamtbdapp.activities;
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.appevents.AppEventsLogger;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.nearby.Nearby;
-import com.google.android.gms.nearby.connection.Connections;
-import com.google.android.gms.nearby.messages.Message;
-import com.google.android.gms.nearby.messages.MessageListener;
-import com.google.android.gms.nearby.messages.PublishCallback;
-import com.google.android.gms.nearby.messages.PublishOptions;
-import com.google.android.gms.nearby.messages.SubscribeCallback;
-import com.google.android.gms.nearby.messages.SubscribeOptions;
 import com.teamtbd.teamtbdapp.R;
 import com.teamtbd.teamtbdapp.events.Bus;
-import com.teamtbd.teamtbdapp.events.TestEvent;
+import com.teamtbd.teamtbdapp.services.ConnectionsService;
 import com.teamtbd.teamtbdapp.services.EventService;
 
 import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
 
-public class HomeActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class HomeActivity extends AppCompatActivity {
     private EventService eventService = new EventService(this);
+    private ConnectionsService connectionsService = new ConnectionsService(this);
     private EventBus eventBus = Bus.getInstance();
-
-    private GoogleApiClient googleApiClient;
-    private MessageListener messageListener;
 
     private TextView testTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final AppCompatActivity currentActivity = this;
-        messageListener = new MessageListener() {
-            @Override
-            public void onFound(Message message) {
-                Toast.makeText(currentActivity, "Received a message! " + new String(message.getContent()), Toast.LENGTH_LONG).show();
-            }
-        };
 
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Nearby.MESSAGES_API)
-                .build();
-
-        if(!googleApiClient.isConnected() && !googleApiClient.isConnecting())
-            googleApiClient.connect();
+        connectionsService.initGoogleAPI();
 
         setContentView(R.layout.activity_home);
-
         testTextView = (TextView)findViewById(R.id.testTextView);
-
 
         Button buttonHost = (Button)findViewById(R.id.buttonHost);
         buttonHost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Intent i = new Intent(currentActivity, HostActivity.class);
-                startActivity(i);*/
-                publish();
+                connectionsService.startAdvertising();
             }
         });
 
@@ -77,98 +42,24 @@ public class HomeActivity extends AppCompatActivity implements GoogleApiClient.C
         buttonClient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Intent i = new Intent(currentActivity, ClientActivity.class);
-                startActivity(i);*/
-                subscribe();
+                connectionsService.startDiscovering();
             }
         });
-
-        //eventService.createEvent("", "");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        eventBus.register(this);
+        //eventBus.register(this);
         AppEventsLogger.activateApp(this);
+        connectionsService.connectToGoogleAPI();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        eventBus.unregister(this);
+        //eventBus.unregister(this);
         AppEventsLogger.deactivateApp(this);
-    }
-
-    @Subscribe
-    public void onTestEvent(TestEvent event) {
-        testTextView.setText(event.content);
-        Log.i("_TEAM_TBD_", "TestEvent received.");
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Log.i("_TEAM_TBD_", "Connected to Google API Services!!!! :D");
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i("_TEAM_TBD_", "Connection to Google API Services suspended!");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i("_TEAM_TBD_", "Connection to Google API Services failed!");
-    }
-
-    private void subscribe() {
-        SubscribeOptions options = new SubscribeOptions.Builder()
-                .setCallback(new SubscribeCallback() {
-                    @Override
-                    public void onExpired() {
-                        super.onExpired();
-                    }
-                }).build();
-
-        Nearby.Messages.subscribe(googleApiClient, messageListener, options)
-                .setResultCallback(new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        if(status.isSuccess())
-                            Log.i("_TEAM_TBD_", "Subscribed successfully.");
-                        else {
-                            Log.i("_TEAM_TBD_", "Could not subscribe.");
-                            Log.i("_TEAM_TBD", "" + status.toString());
-                        }
-                    }
-                });
-    }
-
-    private void publish() {
-        Message message = new Message("test".getBytes());
-
-        PublishOptions options = new PublishOptions.Builder().setCallback(new PublishCallback() {
-            @Override
-            public void onExpired() {
-                super.onExpired();
-            }
-        }).build();
-
-        final AppCompatActivity currentActivity = this;
-        Nearby.Messages.publish(googleApiClient, message, options).setResultCallback(new ResultCallback<Status>() {
-            @Override
-            public void onResult(Status status) {
-                try {
-                    status.startResolutionForResult(currentActivity, 1001);
-                } catch(Exception e) {}
-
-                if(status.isSuccess())
-                    Log.i("_TEAM_TBD_", "Message published.");
-                else {
-                    Log.i("_TEAM_TBD_", "Error.");
-                    Log.i("_TEAM_TBD", "" + status.toString());
-                }
-            }
-        });
+        connectionsService.disconnectFromGoogleAPI();
     }
 }
---
+++ TeamTBD_App/app/src/main/java/com/teamtbd/teamtbdapp/services/ConnectionsService.java
@@ -0,0 +1,178 @@
+package com.teamtbd.teamtbdapp.services;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AppIdentifier;
+import com.google.android.gms.nearby.connection.AppMetadata;
+import com.google.android.gms.nearby.connection.Connections;
+import com.teamtbd.teamtbdapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionsService implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        Connections.ConnectionRequestListener,
+        Connections.EndpointDiscoveryListener,
+        Connections.MessageListener {
+
+    private boolean isHost = false;
+
+    private Context context;
+    private GoogleApiClient googleApiClient;
+
+    public ConnectionsService(Context context) {
+        this.context = context;
+    }
+
+    /* Google API Client */
+    public void initGoogleAPI() {
+        googleApiClient = new GoogleApiClient.Builder(context)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Nearby.CONNECTIONS_API)
+                .build();
+    }
+
+    public void connectToGoogleAPI() {
+        googleApiClient.connect();
+    }
+
+    public void disconnectFromGoogleAPI() {
+        googleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i("_TEAM_TBD_", "Connected to Google API Services.");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("_TEAM_TBD_", "Connection to Google API Services suspended!");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i("_TEAM_TBD_", "Connection to Google API Services failed!");
+    }
+    /* Google API Client */
+
+    /* Host */
+    public void startAdvertising() {
+        isHost = true;
+
+        long NO_TIMEOUT = 0L;
+
+        List<AppIdentifier> appIdentifierList = new ArrayList<>();
+        appIdentifierList.add(new AppIdentifier(context.getPackageName()));
+        AppMetadata appMetadata = new AppMetadata(appIdentifierList);
+
+        String name = null;
+        Nearby.Connections.startAdvertising(googleApiClient, name, appMetadata, NO_TIMEOUT, this)
+                .setResultCallback(new ResultCallback<Connections.StartAdvertisingResult>() {
+                    @Override
+                    public void onResult(Connections.StartAdvertisingResult result) {
+                        if (result.getStatus().isSuccess())
+                            Log.i("_TEAM_TBD_", "Advertising successfully.");
+                        else {
+                            Log.i("_TEAM_TBD_", "Could not startAdvertising.");
+                            Log.i("_TEAM_TBD", "" + result.getStatus().toString());
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionRequest(final String remoteEndpointId, String remoteDeviceId, String remoteEndpointName, byte[] payload) {
+        if(isHost) {
+            byte[] myPayload = null;
+            // Automatically accept all requests
+            final String remoteEndpoint = remoteEndpointName;
+            Nearby.Connections.acceptConnectionRequest(googleApiClient, remoteEndpointId,
+                    myPayload, this).setResultCallback(new ResultCallback<Status>() {
+                @Override
+                public void onResult(Status status) {
+                    if (status.isSuccess()) {
+                        Toast.makeText(context, "Connected to " + remoteEndpoint, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(context, "Failed to connect to: " + remoteEndpoint, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            Nearby.Connections.rejectConnectionRequest(googleApiClient, remoteEndpointId);
+        }
+    }
+    /* Host */
+
+    /* Client */
+    public void startDiscovering() {
+        String serviceId = context.getString(R.string.service_id);
+
+        long DISCOVER_TIMEOUT = 1000L;
+
+        Nearby.Connections.startDiscovery(googleApiClient, serviceId, DISCOVER_TIMEOUT, this)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            Log.i("_TEAM_TBD_", "Discovering successfully.");
+                        } else {
+                            Log.i("_TEAM_TBD_", "Could not startDiscovering.");
+                            Log.i("_TEAM_TBD", "" + status.toString());
+                        }
+                    }
+                });
+    }
+
+    public void connectTo(String endpointId, final String endpointName) {
+        // Send a connection request to a remote endpoint. By passing 'null' for
+        // the name, the Nearby Connections API will construct a default name
+        // based on device model such as 'LGE Nexus 5'.
+        String myName = null;
+        byte[] myPayload = null;
+        Nearby.Connections.sendConnectionRequest(googleApiClient, myName, endpointId, myPayload, new Connections.ConnectionResponseCallback() {
+            @Override
+            public void onConnectionResponse(String remoteEndpointId, Status status, byte[] bytes) {
+                if (status.isSuccess()) {
+                    Toast.makeText(context, "Connected! Endpoint: " + remoteEndpointId, Toast.LENGTH_LONG).show();
+                } else {
+                    Log.e("_TEAM_TBD_", "Could not connect to endpoint.");
+                }
+            }
+        }, this);
+    }
+
+    @Override
+    public void onEndpointFound(final String endpointId, String deviceId, String serviceId, final String endpointName) {
+        Log.i("_TEAM_TBD_", "Endpoint found! " + endpointName);
+        connectTo(endpointId, endpointName);
+    }
+
+    @Override
+    public void onEndpointLost(String s) {
+        Log.e("_TEAM_TBD_", "ENDPOINT LOST ALL. IS. VAIN. I MEAN W T F");
+    }
+
+    @Override
+    public void onMessageReceived(String s, byte[] bytes, boolean b) {
+
+    }
+
+    @Override
+    public void onDisconnected(String s) {
+
+    }
+    /* Client */
+}
