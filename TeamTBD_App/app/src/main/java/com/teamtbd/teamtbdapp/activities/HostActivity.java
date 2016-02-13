53b7632fd2cacc99acce2713182d9ab8af399115
Nicolas Coutu <nico.coutu@hotmail.com>
2016-02-13 11:22:46 -0500

    ajout? hostActivity

--- TeamTBD_App/app/src/main/java/com/teamtbd/teamtbdapp/activities/HostActivity.java
+++ TeamTBD_App/app/src/main/java/com/teamtbd/teamtbdapp/activities/HostActivity.java
@@ -2,17 +2,63 @@ package com.teamtbd.teamtbdapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.teamtbd.teamtbdapp.R;
+import com.teamtbd.teamtbdapp.events.Bus;
+import com.teamtbd.teamtbdapp.events.TitleEvent;
+import com.teamtbd.teamtbdapp.events.TotalTicketEvent;
+import com.teamtbd.teamtbdapp.events.UpdateTotalEvent;
+import com.teamtbd.teamtbdapp.services.EventService;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 public class HostActivity extends AppCompatActivity {
     private String eventId;
+    private int price;
+    private EventBus eventBus = Bus.getInstance();
+    private TextView title, pot;
+    private EventService eventService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
 
+        title = (TextView)findViewById(R.id.eventTittle);
+        pot = (TextView)findViewById(R.id.potValue);
+
         eventId = getIntent().getStringExtra("eventId");
+
+        eventService = new EventService(this);
+        eventService.getTicketPrice(eventId);
+        eventService.getName(eventId);
+        eventService.getTotalTickets(eventId);
+    }
+    @Subscribe
+    public void updatePrice(UpdateTotalEvent event){
+        price = Integer.parseInt(event.content);
+    }
+
+    @Subscribe
+    public void updateTitle(TitleEvent event){
+        title.setText(event.content);
+    }
+    @Subscribe
+    public  void updateTotalTickets(TotalTicketEvent event){
+        pot.setText((Integer.parseInt(event.content) * price) + "$");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        eventBus.unregister(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        eventBus.register(this);
     }
 }
